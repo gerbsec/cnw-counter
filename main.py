27e452c2fdb0d2d62b51060811af12e89f4afa07
gerbsec <albabawaseem@gmail.com>
2022-04-20 15:12:26 -0400

    cool

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 from models import User
 bot = commands.Bot('.')
 users = {}
+words = ['wallah', 'fword', 'sword', 'aword']
+badwords = ['wallah', 'fuck', 'shit', 'ass']
 @bot.event
 async def on_message(message):
     if message.author.id not in users.keys():
@@ -10,13 +12,11 @@ async def on_message(message):
 
 @bot.listen()
 async def on_message(message):
-    if 'wallah' in message.content.lower():
-        users[message.author.id].wallah += 1
-    elif 'fuck' in message.content.lower():
-        users[message.author.id].fword += 1
-    elif 'shit' in message.content.lower():
-        users[message.author.id].sword += 1
-    elif 'ass' in message.content.lower():
-        users[message.author.id].aword += 1
-    
-bot.run('lol')
+    for index, badword in enumerate(badwords):
+        if badword in message.content.lower():
+            users[message.author.id].__dict__[words[index]] += 1
+    for word in words:
+        if users[message.author.id].__dict__[word] >=25:
+            await message.author.send(f"Stop saying {word}, you have said it {users[message.author.id].__dict__[word]} times. Clearing your counter now!")
+            users[message.author.id].__dict__[word] = 0
+bot.run('test') 
