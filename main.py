a89751360bb1d84db95999bc5803f531b6be0ab7
gerbsec <albabawaseem@gmail.com>
2022-04-22 16:25:30 -0700

    Some functional changes

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from discord.ext import commands
 from models import User
 bot = commands.Bot('.')
 users = {}
-words = ['wallah', 'fword', 'sword', 'aword']
-badwords = ['wallah', 'fuck', 'shit', 'ass']
+words = []
+badwords = []
 @bot.event
 async def on_message(message):
     if message.author.id not in users.keys():
@@ -15,8 +15,11 @@ async def on_message(message):
     for index, badword in enumerate(badwords):
         if badword in message.content.lower():
             users[message.author.id].__dict__[words[index]] += 1
-    for word in words:
-        if users[message.author.id].__dict__[word] >=25:
-            await message.author.send(f"Stop saying {word}, you have said it {users[message.author.id].__dict__[word]} times. Clearing your counter now!")
-            users[message.author.id].__dict__[word] = 0
-bot.run('test') 
+            users[message.author.id].total += 1
+    if users[message.author.id].total >= 10:
+        msg = "You have been saying some bad words, please stop, those words include: \n"
+        for index, word in enumerate(words):
+            msg += f"{word}: {users[message.author.id].__dict__[word]}\n"
+        await message.author.send(msg)
+        users[message.author.id].__dict__[word] = 0
+bot.run('lol')
--- models.py
+++ models.py
@@ -4,7 +4,5 @@ from dataclasses import dataclass
 @dataclass(frozen=False)
 class User:
     name: str
-    wallah: int = 0
-    fword: int = 0
-    sword: int = 0
-    aword: int = 0
+    total: int = 0
+
